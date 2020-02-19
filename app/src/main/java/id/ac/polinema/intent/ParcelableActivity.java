5945ea290be4f3a7aa41a7eaebaae64a4141490b
Nisauz Zahroul Aini <nisauzahr@gmail.com>
2020-02-19 23:28:55 +0700

    Intent Demo

--- app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -1,15 +1,50 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import id.ac.polinema.intent.Model.User;
+
+import static id.ac.polinema.intent.BundleActivity.AGE_KEY;
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
+
 public class ParcelableActivity extends AppCompatActivity {
+
+    private static final String USER_KEY = "user";
+
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
 
+
+    public void handleSubmit(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+
+        //mendeklarasikan variabel user
+        User user = new User(username, name, age);
+        Intent intent = new Intent(this, ProfileBundleActivity.class);
+        intent.putExtra(USER_KEY, user);
+
+        startActivity(intent);
+    }
 }
